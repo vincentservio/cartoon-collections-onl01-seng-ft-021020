5034f75119fcb2ddb166c93fa2db04f36faee9be
Vincent Servio <vincentservio@gmail.com>
2020-02-13 04:30:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,5 +35,6 @@ def find_the_cheese(cheddar_cheese)  # code an argument here
   # the array below is here to help
   binding.pry]
   cheese_types = ["cheddar", "gouda", "camembert"]
-   if cheddar_cheese.include?("cheddar", "gouda", "camembert")
+  
+  # if cheddar_cheese.include?("cheddar", "gouda", "camembert")
 end
